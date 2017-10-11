edc8831151243f400f7e83493bc0600cf44ae183
bmccoy04 <bryanmccoy04@gmail.com>
2017-10-11 15:35:00 -0500

    Some basic class creation.

--- src/CoreXmlParser/CoreXmlParser.cs
+++ src/CoreXmlParser/CoreXmlParser.cs
@@ -17,4 +17,26 @@ namespace CoreXmlParser
             _content = contentToParse;
         }
     }
+
+    public class Node 
+    {
+        public NodeList Children { get; set; }
+
+        public NodeAttributeList NodeAttributeList {get;set;}
+    }
+
+    public class NodeAttributeList
+    {
+        public NodeAttribute Current { get; set; }
+    }
+
+    public class NodeAttribute 
+    {
+
+    }
+
+    public class NodeList
+    {
+
+    }
 }
